85c77563ea680a26b80f96f48687ea960b150b2b
Will Thompson <wjt@endlessm.com>
2020-09-10 11:21:05 +0100

    Add © and MPL 2.0 notice to C♯ source files
    Some of the .cs files in this repository are generated, so to skip them
    I used:
    
        find -name '*.cs' |
        xargs grep --files-without-match 'was generated' |
        xargs python3 add-header
    
    'add-header' is the following script, which handles the Windows
    convention, followed by these source files, of prepending U+FEFF (byte
    order mark) to UTF-8 text files (even though UTF-8 does not have
    variable byte order):
    
        #!/usr/bin/python3
        import argparse
    
        def main():
            parser = argparse.ArgumentParser()
            parser.add_argument(
                "files", nargs="+", type=argparse.FileType(mode="r+", encoding="utf-8-sig"),
            )
            args = parser.parse_args()
    
            with open("HEADER", "r") as f:
                header = f.readlines()
    
            for f in args.files:
                source = f.readlines()
                f.seek(0)
                f.writelines(header + source)
    
        if __name__ == "__main__":
            main()
    
    HEADER is the following:
    
        // © 2019–2020 Endless OS Foundation LLC
        //
        // This file is part of Endless Launcher.
        //
        // This Source Code Form is subject to the terms of the Mozilla Public
        // License, v. 2.0. If a copy of the MPL was not distributed with this
        // file, You can obtain one at http://mozilla.org/MPL/2.0/.
    
    MPL-2.0 is chosen rather than a license from the GPL family because one
    of this project's dependencies, CommonServiceLocator, is released under
    the Microsoft Public License (MS-PL), which the FSF deems incompatible
    with the GPL.
    
    https://phabricator.endlessm.com/T30706

--- EndlessLauncher/model/SystemVerificationException.cs
+++ EndlessLauncher/model/SystemVerificationException.cs
@@ -1,4 +1,11 @@
-﻿namespace EndlessLauncher.model
+﻿// © 2019–2020 Endless OS Foundation LLC
+//
+// This file is part of Endless Launcher.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+namespace EndlessLauncher.model
 {
     public class SystemVerificationException : EndlessExceptionBase<SystemVerificationErrorCode>
     {
--- EndlessLauncher/service/IFrameNavigationServiceBase.cs
+++ EndlessLauncher/service/IFrameNavigationServiceBase.cs
@@ -1,4 +1,11 @@
-﻿using GalaSoft.MvvmLight.Views;
+﻿// © 2019–2020 Endless OS Foundation LLC
+//
+// This file is part of Endless Launcher.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+using GalaSoft.MvvmLight.Views;
 
 namespace EndlessLauncher.service
 {
