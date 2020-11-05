b3628d8e47ec6a8b1e26aab3b8d2aa1cf3e300cd
Damián Nohales <damiannohales@gmail.com>
2020-11-05 18:20:38 -0300

    Add Makefile based Shortcuts projects to replace [Kiwix|Kolibri]Shortcut projects

---
+++ Shortcuts/main.c
@@ -0,0 +1,11 @@
+#include <Windows.h>
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                      _In_opt_ HINSTANCE hPrevInstance,
+                      _In_ LPWSTR    lpCmdLine,
+                      _In_ int       nCmdShow)
+{
+    ShellExecuteW(NULL, NULL, TEXT("Endless Launcher.exe"), TEXT(WUL_LAUNCHER_FLAGS), NULL, 0);
+    return 0;
+}
+
