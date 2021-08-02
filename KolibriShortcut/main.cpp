d1c13e304f568fd0992717ded73cf0c4a565f8ac
Daniel García Moreno <dani@danigm.net>
2021-08-02 13:18:11 +0200

    Use kolibri VCRUNTIME140.dll
    https://phabricator.endlessm.com/T32361

--- KolibriShortcut/main.cpp
+++ KolibriShortcut/main.cpp
@@ -1,10 +1,15 @@
 #include <Windows.h>
+#include <stdio.h>
+
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                       _In_opt_ HINSTANCE hPrevInstance,
                       _In_ LPWSTR    lpCmdLine,
                       _In_ int       nCmdShow)
 {
+    printf("Launching\n");
+    AddDllDirectory(L"D:\\.kolibri-windows\\resources\\app\\src\\Kolibri");
+    printf("After setting DLL\n");
     ShellExecuteW(NULL, NULL, TEXT("kolibri-electron.exe"), NULL, TEXT(".kolibri-windows"), 0);
     return 0;
 }
